5b274a8bee846fba00b644b57a7e551ea9408995
shafahadshah <pirshahfahadshah@gmail.com>
2026-02-24 17:10:32 +0500

    connect user routes and complete CRUD API

--- 26-express-users-crud-api/server.js
+++ 26-express-users-crud-api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('REST CRUD API Running...');
