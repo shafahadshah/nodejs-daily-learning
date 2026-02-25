0d5085573ff3e0318a2d086d45a08764bf73de73
shafahadshah <pirshahfahadshah@gmail.com>
2026-02-25 17:13:28 +0500

    register routes and 404 handler

--- 27-restful-api/src/app.js
+++ 27-restful-api/src/app.js
@@ -1,6 +1,17 @@
 import express from "express";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
+
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found"
+  });
+});
 
 export default app;
