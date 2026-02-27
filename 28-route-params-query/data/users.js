e74c6eeaf81bc07294a2c912289e594f7e0b51b4
shafahadshah <pirshahfahadshah@gmail.com>
2026-02-27 17:14:40 +0500

    Add mock users data

---
+++ 28-route-params-query/data/users.js
@@ -0,0 +1,8 @@
+const users = [
+  { id: 1, name: "John", age: 25 },
+  { id: 2, name: "Alice", age: 30 },
+  { id: 3, name: "Bob", age: 22 },
+  { id: 4, name: "David", age: 35 }
+];
+
+module.exports = users;
