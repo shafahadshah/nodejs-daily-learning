58096c45bc12fca377c6aa7f4527ce974821cfbd
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-01 17:03:20 +0500

    Connect Products Router & Add 404 Handler

--- 30-express-router-modular/app.js
+++ 30-express-router-modular/app.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-// Mount User Routes
+// Modular Route Mounting
 app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
 // Root Route
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
-    message: "Express Router Modular Application"
+    message: "Express Router - Modular Route Handling"
+  });
+});
+
+// 404 Handler
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found"
   });
 });
 
