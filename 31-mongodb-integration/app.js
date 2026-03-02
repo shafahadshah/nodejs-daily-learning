1e2e0e1a5755589c1fa111f77dfa2aa6fb26ff8d
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-02 17:04:08 +0500

    Connect Database Before Server Start

--- 31-mongodb-integration/app.js
+++ 31-mongodb-integration/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const PORT = 5000;
@@ -9,6 +10,12 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+};
+
+startServer();
