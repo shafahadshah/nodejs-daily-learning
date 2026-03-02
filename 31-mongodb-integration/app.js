31e8ccecd6c5d6c0fa05f3134e1313827e658865
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-02 17:10:10 +0500

    Integrate Routes & Database Integration

--- 31-mongodb-integration/app.js
+++ 31-mongodb-integration/app.js
@@ -1,14 +1,13 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use("/api/users", userRoutes);
 
 const startServer = async () => {
   await connectDB();
