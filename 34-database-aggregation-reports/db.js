4909900768a1032b7ed758e27d3d10cb8b005be3
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-05 14:26:32 +0500

    Connect DB for aggregation pipeline

--- 34-database-aggregation-reports/db.js
+++ 34-database-aggregation-reports/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
-async function connectDB() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/aggregation_demo");
-  console.log("DB connected");
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/aggregation_demo");
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.log("DB connection error");
+  }
+};
 
-module.exports = connectDB;                             
+module.exports = connectDB;
