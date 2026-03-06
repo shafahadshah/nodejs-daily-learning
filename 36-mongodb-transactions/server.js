ba48df61cff3c7e883af1c435289722387c5d92c
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-06 14:23:23 +0500

    connect routes and database

--- 36-mongodb-transactions/server.js
+++ 36-mongodb-transactions/server.js
@@ -1,11 +1,14 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
+import accountRoutes from "./routes/accountRoutes.js";
 
 const app = express();
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("MongoDB Transactions Demo");
-});
+connectDB();
+
+app.use("/api", accountRoutes);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
