7d63f5462203d78835ee4ecb9434339b9fe3d9ad
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-07 14:20:40 +0500

    add login controller with password verification

--- 37-user-authentication/controllers/authController.js
+++ 37-user-authentication/controllers/authController.js
@@ -32,3 +32,32 @@ exports.signup = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    res.json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
