f6420f334ac44408b7a3ce21bb171f41dc3d7dfe
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-07 14:14:19 +0500

    implement signup controller with password hashing

---
+++ 37-user-authentication/controllers/authController.js
@@ -0,0 +1,34 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const generateToken = require("../utils/generateToken");
+
+exports.signup = async (req, res) => {
+  try {
+
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
