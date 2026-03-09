964a5ed9cc8a48af19d1ee63acbedd060933624e
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-09 14:17:50 +0500

    create jwt auth middleware

---
+++ 39-jwt-auth-protect-routes/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken")
+
+const SECRET = "secret123"
+
+module.exports = (req, res, next) => {
+  const header = req.headers.authorization
+
+  if (!header) {
+    return res.status(401).json({ message: "No token provided" })
+  }
+
+  const token = header.split(" ")[1]
+
+  try {
+    const decoded = jwt.verify(token, SECRET)
+    req.user = decoded
+    next()
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" })
+  }
+}
