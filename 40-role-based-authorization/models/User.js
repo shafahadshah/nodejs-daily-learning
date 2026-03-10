dfc9e3edc052e02f79b3f1a9d0aa64aa16eb83ce
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-10 14:53:41 +0500

    create user model roles

---
+++ 40-role-based-authorization/models/User.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    unique: true
+  },
+  password: String,
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user"
+  }
+});
+
+export default mongoose.model("User", userSchema);
