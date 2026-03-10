f3fe5de4100777db60b46095280db3842b84e544
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-10 14:57:59 +0500

    create authentication routes

---
+++ 40-role-based-authorization/routes/authRoutes.js
@@ -0,0 +1,43 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashed,
+    role
+  });
+
+  res.json(user);
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) return res.status(400).json({ message: "Invalid password" });
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+
+  res.json({ token });
+});
+
+export default router;
