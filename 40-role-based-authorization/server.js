cbd8a92220bdc88a75adb8ed0cb9008faf7a1520
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-10 14:50:10 +0500

    create express server setup

---
+++ 40-role-based-authorization/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
