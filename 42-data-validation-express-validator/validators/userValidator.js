143f3e44520d9e9cedfd1ad351d410305fddb8f7
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-12 14:14:49 +0500

    Create validation middleware rules

---
+++ 42-data-validation-express-validator/validators/userValidator.js
@@ -0,0 +1,15 @@
+const { body } = require("express-validator");
+
+exports.userValidationRules = [
+  body("name")
+    .notEmpty()
+    .withMessage("Name is required"),
+
+  body("email")
+    .isEmail()
+    .withMessage("Valid email required"),
+
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters")
+];
