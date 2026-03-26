430b9178da70ebad6de6bc419c1e8896bfccce68
shafahadshah <pirshahfahadshah@gmail.com>
2026-03-26 15:12:22 +0500

    setup app config

---
+++ 55-integration-testing-api/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const userRoutes = require("./routes.user");
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/users", userRoutes);
+
+module.exports = app;
