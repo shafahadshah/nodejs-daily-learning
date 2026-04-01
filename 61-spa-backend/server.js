b3372a8e50c4acba57494dfe4ae87cd19b0de4c5
shafahadshah <pirshahfahadshah@gmail.com>
2026-04-01 22:34:27 +0500

    create express server

---
+++ 61-spa-backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from backend!" });
+});
+
+app.use(express.static(__dirname));
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
